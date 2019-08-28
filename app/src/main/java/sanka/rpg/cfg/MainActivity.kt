ac82c82cea208f2f96fbdbd26989150b018273e5
sanka01 <sanka1375@gmail.com>
2019-08-28 13:19:55 -0300

    initial commit

---
+++ app/src/main/java/sanka/rpg/cfg/MainActivity.kt
@@ -0,0 +1,19 @@
+package sanka.rpg.cfg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnContinuar : Button = findViewById(R.id.buttonContinuar)
+
+        btnContinuar.setOnClickListener {
+
+        }
+    }
+}
